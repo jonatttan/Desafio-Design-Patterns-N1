c33322e016c0fa30542628dd85a28f20005c1ceb
Jonattan <jonattan.sousa@gmail.com>
2021-05-18 18:00:16 -0300

    Primeiro commit do verificador de CPF/ CNPJ.

---
+++ DesafioDesignPatterns-N1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DesafioDesignPatterns-N1
+//
+//  Created by Jonattan Moises Sousa on 18/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+## Desafio Design Patterns N1
+
+### Do que se trata:
+
+Um programa capaz de informar se um número de CPF ou CNPJ é válido.
